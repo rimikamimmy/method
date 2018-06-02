0bd23ad0d505953a8b6959df18177ce64d1863d8
RIMIKA <rimikamimmy727@gmail.com>
2018-06-02 16:26:51 +0900

    method 追加課題

--- method/ViewController.swift
+++ method/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var keisan: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+//
+//    @IBAction func resultButton(sender: UIButton) {
+//        var x1 = tasizan1.text.toInt()!
+//        var x2 = tasizan2.text.toInt()!
+//
+//        var y = x1 + x2
+//
+//        result.text = String(y)
+    @IBAction func keisanbutton(){
+        var x1 = Int(text1.text!)
+        var x2 = Int(text2.text!)
+        
+        var y = x1! + x2!
+        
+        label.text = String(y)
+        
     }
 
 
